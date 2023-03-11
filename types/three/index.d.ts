b1ce35d390d97869baa462fe9005847d83434705
Yaroslav Larin <cyberbiont@gmail.com>
2023-03-11 18:00:53 +0200

    disable global namespace export

--- types/three/index.d.ts
+++ types/three/index.d.ts
@@ -7,8 +7,4 @@
 
 export * from './src/Three';
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace THREE;
+// export as namespace THREE;
